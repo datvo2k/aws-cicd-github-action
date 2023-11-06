8131bb01176ef85bc6d9cd554481929170ba7b01
Brian Vo <datvo2k@gmail.com>
2023-11-07 00:55:50 +0700

    push terraform module

---
+++ src/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-state"
+    # key    = "dev/vpc.tfstate"
+    region         = "us-west-1"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
